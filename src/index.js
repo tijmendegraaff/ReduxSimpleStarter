6e6e706ad3a2b545432d22e4c37f50c645e50664
Tijmen de Graaff <tijmen76@gmail.com>
2017-08-11 01:42:44 +0200

    add handler to search-input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar"
 
 const API_KEY = "AIzaSyA-ogQBTivY9Hy7VcweFw_nQWgHPdQAWgs";
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+    <div>
+        <SearchBar />
+    </div>)
+;
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));

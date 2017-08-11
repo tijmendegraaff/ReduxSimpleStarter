3261797aa81f1f3ea8ab86c3ebd0ee2ea63c2f92
Tijmen de Graaff <tijmen76@gmail.com>
2017-08-11 15:54:22 +0200

    add search function

--- src/index.js
+++ src/index.js
@@ -15,18 +15,22 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch("surfboards");
+  }
 
-    YTSearch({ key: API_KEY, term: "surfboard" }, videos => {
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           videos={this.state.videos}
